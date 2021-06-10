07e883bb6b120337a3f8bef5d8e83d0024a7fc1f
Muhammad Ramadhan <mr.maulanaasyarief@gmail.com>
2021-06-10 20:55:59 +0700

    CustomerXSupplier

--- renovasi_rumah.sql
+++ renovasi_rumah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2021 at 06:59 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Jun 03, 2021 at 07:30 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -75,10 +75,12 @@ INSERT INTO `customer` (`id_customer`, `nama`, `alamat`, `no_hp`) VALUES
 CREATE TABLE `detail_trans_supplier` (
   `id_detail_trans` varchar(15) NOT NULL,
   `id_transaksi` varchar(15) NOT NULL,
-  `id_supplier` varchar(15) NOT NULL,
   `harga` int(11) NOT NULL,
   `total_diskon` int(11) NOT NULL,
-  `total_bayar` int(11) NOT NULL
+  `total_bayar` int(11) NOT NULL,
+  `id_material` varchar(15) DEFAULT NULL,
+  `item` varchar(100) DEFAULT NULL,
+  `volume` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -114,12 +116,20 @@ CREATE TABLE `material` (
 --
 
 CREATE TABLE `pegawai` (
-  `id_pegawai` int(100) NOT NULL,
+  `id_pegawai` int(11) NOT NULL,
   `nama_pegawai` varchar(100) NOT NULL,
   `alamat_pegawai` varchar(100) NOT NULL,
-  `telp_pegawai` int(11) NOT NULL,
+  `telp_pegawai` varchar(13) NOT NULL,
   `jenis_pegawai` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`id_pegawai`, `nama_pegawai`, `alamat_pegawai`, `telp_pegawai`, `jenis_pegawai`) VALUES
+(1, 'getty', 'subang', '2147483647', ''),
+(2, 'regina', 'serang', '087653428197', 'balkon');
 
 -- --------------------------------------------------------
 
@@ -214,9 +224,30 @@ CREATE TABLE `pemesanan_supplier` (
   `ongkos_kirim` int(11) NOT NULL,
   `total_jual` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
   `alamat_kirim` varchar(100) NOT NULL,
   `status_bayar` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemesanan_supplier`
+--
+
+INSERT INTO `pemesanan_supplier` (`id_pemesanan`, `id_supplier`, `tanggal_pesan`, `tanggal_ambil`, `diskon`, `ongkos_kirim`, `total_jual`, `harga`, `jumlah`, `alamat_kirim`, `status_bayar`) VALUES
+(15, 7, '2021-04-20', '2021-04-21', '100000', 10000, 990000, 1000000, 0, 'serang', 'Belum Lunas'),
+(16, 8, '2021-04-08', '2021-04-29', '50000', 10000, 460000, 500000, 0, 'bandung', 'Belum Lunas'),
+(17, 8, '2021-04-08', '2021-04-29', '50000', 10000, 460000, 500000, 0, 'bandung', 'Belum Lunas'),
+(18, 8, '2021-04-22', '2021-04-29', '50000', 20000, 770000, 700000, 0, 'Bojongsoang', 'Belum Lunas'),
+(19, 8, '2021-04-01', '2021-04-20', '50000', 10000, 960000, 1000000, 0, 'Bojongsoang', 'Belum Lunas'),
+(20, 8, '2021-04-01', '2021-04-20', '50000', 10000, 960000, 1000000, 0, 'Bojongsoang', 'Belum Lunas'),
+(21, 11, '2021-04-13', '2021-04-27', '100000', 20000, 920000, 1000000, 0, 'Telkom', 'Lunas'),
+(22, 11, '2021-04-18', '2021-04-21', '200000', 150000, 9950000, 10000000, 0, 'Aceh', 'Belum Lunas'),
+(23, 11, '2021-04-18', '2021-04-21', '200000', 150000, 9950000, 10000000, 0, 'Aceh', 'Belum Lunas'),
+(24, 11, '2021-04-13', '2021-04-20', '10000', 20000, 510000, 500000, 0, 'Cimuncang', 'Lunas'),
+(25, 11, '2021-04-19', '2021-04-21', '10000', 5000, 695000, 700000, 0, 'Subang', 'Lunas'),
+(26, 7, '2021-04-20', '2021-04-22', '10000', 20000, 1010000, 1000000, 0, 'Lampung', 'Belum Lunas'),
+(27, 14, '2021-04-20', '2021-04-22', '100000', 10000, 910000, 1000000, 0, 'serang', 'Belum Lunas'),
+(28, 16, '2021-04-01', '2021-04-07', '100000', 10000, 910000, 500000, 0, 'Cimuncang', 'Belum Lunas');
 
 -- --------------------------------------------------------
 
@@ -229,19 +260,37 @@ CREATE TABLE `supplier` (
   `nama_supplier` varchar(100) NOT NULL,
   `alamat_supplier` varchar(100) NOT NULL,
   `telepon_supplier` varchar(13) NOT NULL,
-  `jenis_material` varchar(100) NOT NULL
+  `jenis_material` varchar(100) NOT NULL,
+  `harga_material` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `supplier`
 --
 
-INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `alamat_supplier`, `telepon_supplier`, `jenis_material`) VALUES
-(7, 'Syafia', 'Ciracas', '081221065578', 'Batu granit'),
-(8, 'Alyssa', 'Pabuaran', '08122106954', 'Keramik marbel'),
-(9, 'Regina', 'Serang', '081221076646', 'cat'),
-(10, 'Biyan', 'Ciceri', '081221076644', 'besi'),
-(11, 'Doni', 'jakarta', '081221076645', 'baja');
+INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `alamat_supplier`, `telepon_supplier`, `jenis_material`, `harga_material`) VALUES
+(7, 'Syafia', 'Ciracas', '081221065578', 'Batu granit', 0),
+(8, 'Alyssa', 'Pabuaran', '08122106954', 'Keramik marbel', 0),
+(9, 'Regina', 'Serang', '081221076646', 'cat', 0),
+(10, 'Biyan', 'Ciceri', '081221076644', 'besi', 0),
+(11, 'Doni', 'jakarta', '081221076645', 'baja', 0),
+(12, 'Davin', 'Lontar', '081221067734', 'Kayu Jati', 0),
+(13, 'Rege', 'Sukabirus', '082116574423', 'Kawat', 0),
+(14, 'Jihan', 'Batam', '089441675529', 'Rumput', 0),
+(16, 'getty', 'subang', '081221076646', 'cat tembok', 500000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_customer`
+--
+
+CREATE TABLE `transaksi_customer` (
+  `id_trans_customer` varchar(15) NOT NULL,
+  `id_customer` varchar(15) NOT NULL,
+  `jenis_renovasi` varchar(100) NOT NULL,
+  `total_renovasi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -388,6 +437,13 @@ ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
+-- Indexes for table `transaksi_customer`
+--
+ALTER TABLE `transaksi_customer`
+  ADD PRIMARY KEY (`id_trans_customer`),
+  ADD KEY `id_customer` (`id_customer`);
+
+--
 -- Indexes for table `transaksi_jasa_desain`
 --
 ALTER TABLE `transaksi_jasa_desain`
@@ -457,13 +513,13 @@ ALTER TABLE `pemesanan`
 -- AUTO_INCREMENT for table `pemesanan_supplier`
 --
 ALTER TABLE `pemesanan_supplier`
-  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
-  MODIFY `id_supplier` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_supplier` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
